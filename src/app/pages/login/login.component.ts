5c9f819f36a7d8f5de28c2ae54e130160a69767a
Akio <akioandreimat@gmail.com>
2024-09-18 22:22:15 -0300

    #21777 - Criação da tela de Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule, FontAwesomeModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
   loginForm: FormGroup;
-  passwordFieldType: string = 'password';
+  passwordFieldType: boolean = true;
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
@@ -28,6 +31,6 @@ export class LoginComponent {
   }
 
   togglePasswordVisibility(): void {
-    this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
+    this.passwordFieldType = !this.passwordFieldType
   }
 }
